899b9e47c021d2e557d4b6c39caf8a4a8b0a9f26
Muhika Thomas <tommuhyka@hotmail.com>
2016-01-10 21:41:11 +0300

    Checking for connection using socket module

---
+++ Completed tests/main.py
@@ -0,0 +1,33 @@
+'''
+Author = Mossein King
+Alias = --Mk--
+
+This script is open soure and of so open to editing and use as you wish
+This script is dependent on socket module
+This module provides access to the BSD socket interface. It is available on all modern Unix systems, Windows, MacOS, and probably additional platforms.
+Before using this script first install soket using 
+pip install socket
+if you do not have it  
+'''
+import sys
+import socket
+from subprocess import call, PIPE, STDOUT
+
+def main():	
+	print connection_test()
+	
+def connection_test():
+	locale = "www.google.com"
+	try:
+		host = socket.gethostbyname(locale)
+    	# connect to the host
+		s = socket.create_connection((host, 80), 2)
+		connected = "Cheching internet access ... TRUE"
+		return connected
+	except:
+		pass
+		unconnected = "Cheching internet access ... FALSE\n Check your internet connection"
+		return unconnected
+
+if __name__ =='__main__':
+	main()
