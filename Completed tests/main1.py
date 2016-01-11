c3698fbccd2366b6838b1f27edec97b7b2e9a4f7
Muhika Thomas <tommuhyka@hotmail.com>
2016-01-11 23:29:58 +0300

    Using urllib

--- Completed tests/main1.py
+++ Completed tests/main1.py
@@ -6,7 +6,7 @@ This script is open soure and of so open to editing and use as you wish
 This script is dependent on urllib module
 This module provides a high-level interface for fetching data across the World Wide Web.
 The module comes integrated in python
-This method is unpreferable since urllib provides a high-level interface and hence  may not dedect very slow connections  
+This method is not preferable since urllib provides a high-level interface and hence  may not dedect very slow connections  
 '''
 import urllib
 
